4438862d2c2d9bd9427646ae41e678ee2646ad7b
David Guttman <david@davidguttman.com>
2018-06-16 07:32:49 -0700

    fiat lux

---
+++ index.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+var split = require('split2')
+var through = require('through2')
+
+var argv = require('yargs')
+  .option('indent', {
+    alias: 'i',
+    describe: 'json indent size',
+    default: 0
+  })
+  .argv
+
+var indent = argv.indent
+
+process.stdin
+  .pipe(split())
+  .pipe(through(function (chunk, enc, cb) {
+    var str = chunk.toString()
+    var i = str.indexOf('{')
+    if (i < 0) return cb(null, chunk + '\n')
+
+    var json = str.slice(i)
+    if (!indent) return cb(null, json + '\n')
+
+    var obj = JSON.parse(json)
+    cb(null, JSON.stringify(obj, null, indent) + '\n')
+  }))
+  .pipe(process.stdout)
